7bf1fb6fb5140b52b487993d66bd5accd1bd289b
Caian Egidio <caianpedroso@gmail.com>
2024-04-21 22:56:50 -0300

    test lib html2pdf

---
+++ src/app/app.style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+export const Wrapper = styled.main`
+  min-height: 100vh;
+  .content {
+      background-color: red;
+  }
+  padding: 0 3rem;
+  .title {
+    margin-bottom: 10px;
+    color: #c1c1c1;
+  }
+  .btn__container {
+    margin-top: 10px;
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
