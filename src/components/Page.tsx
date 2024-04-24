a4ec9371eb2b140944bbd00181165b010a353bc0
Caian Egidio <caianpedroso@gmail.com>
2024-04-23 22:30:51 -0300

    remove libs

--- src/components/Page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Page.module.css'
-
-type Props = {
-    children: React.ReactNode
-}
-
-const Page = ({ children }: Props) => <div className={styles.page}>{children}</div>
-
-export default Page
