3071429ead0da80a7d1394ca7427101718bd0f44
Caian Egidio <caianpedroso@gmail.com>
2024-05-16 23:49:31 -0300

    update

--- src/app/app.style.ts
+++ src/app/app.style.ts
@@ -1,17 +1,1 @@
 import styled from "styled-components";
-export const Wrapper = styled.main`
-  min-height: 100vh;
-  .content {
-      background-color: red;
-  }
-  padding: 0 3rem;
-  .title {
-    margin-bottom: 5px;
-    color: #c1c1c1;
-  }
-  .btn__container {
-    margin-top: 10px;
-    display: flex;
-    justify-content: flex-end;
-  }
-`;
